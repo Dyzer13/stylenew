7499e9ecece82fff43659031e1c959eb02a21705
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2019-03-17 05:39:25 -0700

    Update music.py

--- music.py
+++ music.py
@@ -6,12 +6,5 @@ from discord.utils import find
 import requests as rq
 
 bot_token=os.environ['BOT_TOKEN']
-client = commands.bot(command_prefix = '+')
-
-@client.event
-async def on_ready()=
- await client.change.presence(game=discord.Game(name='Test'))
- print('bot is ready')
- 
 
 bot.run('bot_token')
