d9924a9622a5e4ab82b1814ebaedc3fc18ad2c70
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2019-03-20 03:31:41 -0700

    Update music.py

--- music.py
+++ music.py
@@ -1,10 +1,11 @@
 import discord
 from discord.ext import commands
 
-bot = commands.bot(description="blablabla", command_prefix="&")
+bot = commands.Bot(command_prefix='>')
 
 @bot.command()
-async def ping (ctx):
-     await ctx.send('Pong!')
+async def ping(ctx):
+    await ctx.send('pong')
+
 
 bot.run('NTU3ODM5ODcxNzQyNTc0NjE0.D3ONFQ.bw60zevXJO-k28s08Te_aeWh_qo')
