162844e9d8c2c227b0a0e511680f2c368bad19fb
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2019-03-20 02:15:44 -0700

    Update music.py

--- music.py
+++ music.py
@@ -4,18 +4,5 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='>')
 
 
-@bot.command()
-async def ping():
-     await ctx.send('Pong!')
-@bot.command()
-async def mul(ctx, num1: int, num2: int):
-     await ctx.send(num1*num2)
-
-@bot.event
-async def on_ready():
-    game = discord.Game("with the Api")
-    await bot.change_presence(status=discord.Status.idle, activity=game)
-
-
 
 bot.run('NTU3ODM5ODcxNzQyNTc0NjE0.D3ONFQ.bw60zevXJO-k28s08Te_aeWh_qo')
