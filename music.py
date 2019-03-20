8de5abdbb0a0e926206b9a73b086dc694d38084a
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2019-03-20 01:48:10 -0700

    Update music.py

--- music.py
+++ music.py
@@ -7,8 +7,4 @@ async def on_ready():
  await client.change.presence(game=discord.Game(name='Test'))
  print('bot is ready')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
 bot.run('NTU3ODM5ODcxNzQyNTc0NjE0.D3ONFQ.bw60zevXJO-k28s08Te_aeWh_qo')
