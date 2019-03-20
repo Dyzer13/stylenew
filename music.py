40f3f5b45602ce0eef3b6d25048b3dbeed30fc6e
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2019-03-20 03:27:10 -0700

    Update music.py

--- music.py
+++ music.py
@@ -1,10 +1,9 @@
 import discord
 from discord.ext import commands
+bot = commands.bot(description="blablabla", command_prefix"&")
 
-bot = commands.Bot(command_prefix='>')
+@bot.command()
+async def ping (ctx):
+     await ctx.send('Pong!')
 
-
-@bot.event()
-async def on_ready():
-    await bot.change_presence(activity=discord.streaming(name="waiting", url="https://www.twitch.tv/clow")) 
 bot.run('NTU3ODM5ODcxNzQyNTc0NjE0.D3ONFQ.bw60zevXJO-k28s08Te_aeWh_qo')
