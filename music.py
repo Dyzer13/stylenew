61df04a834e3fa7c4cea8e6dbd1ac6303b0592ea
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2019-03-20 03:11:54 -0700

    Update music.py

--- music.py
+++ music.py
@@ -6,5 +6,5 @@ bot = commands.Bot(command_prefix='>')
 
 @bot.me()
 async def on_ready():
-    await bot.change_presence(activity=discord.streaming(name="waiting", url="https://www.twitch.tv/clow") 
+    await bot.change_presence(activity=discord.streaming(name="waiting", url="https://www.twitch.tv/clow")) 
 bot.run('NTU3ODM5ODcxNzQyNTc0NjE0.D3ONFQ.bw60zevXJO-k28s08Te_aeWh_qo')
