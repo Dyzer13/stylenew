874d61e2d6d19487a45234d6943b2d7a8d60eb63
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2019-03-20 03:38:18 -0700

    Update music.py

--- music.py
+++ music.py
@@ -7,5 +7,16 @@ bot = commands.Bot(command_prefix='>')
 async def ping(ctx):
     await ctx.send('pong')
 
+    
+    @client.command(pass_context=True)
+async def kick(ctx, member: discord.Member = None):
+    channel = ctx.message.channel
+    message = ctx.message
+    if not member:
+        await client.send_message(ctx.message.channel, 'Please specify a member')
+        return
+    await client.kick(member)
+    await client.say('{} got kicked'.format(member.mention))
+    await client.delete_message(message)
 
 bot.run('NTU3ODM5ODcxNzQyNTc0NjE0.D3ONFQ.bw60zevXJO-k28s08Te_aeWh_qo')
