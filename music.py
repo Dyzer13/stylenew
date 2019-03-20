d73df3a735457b4cf30d4935458fe1444da253ec
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2019-03-20 03:42:31 -0700

    Update music.py

--- music.py
+++ music.py
@@ -8,36 +8,14 @@ async def ping(ctx):
     await ctx.send('pong')
 
     
-client = Client()
-
-COMMANDS = [
-                "!Version",
-                "!Owner",
-           ]
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-
-    elif message.content.startswith('!Owner'):
-        embed = discord.Embed(title="The Owner is!", description="Chrome Panda", color=0x00ff00)
-        await client.send_message(message.channel, embed=embed)
-    elif message.content.startswith('!Version'):
-        embed = discord.Embed(title="Version for this Bot is", description="Virsion = 1.0", color=0x00ff00)
-        await client.send_message(message.channel, embed=embed)
-    elif message.content.startswith('!COMMANDS'):
-        embed = discord.Embed(title="Commands Are", description="Version, Owner", color=0x00ff00)
-        await client.send_message(Message.channel, embed=embed)
-
-
-
-@client.event
-async def on_ready():
-    print(client.user.name)
-    print(client.user.id)
-    print("PandaBot is ready to use!\n\nHave Fun!")
-    client.close()
+ot.command()
+async def names(ctx, name):
+    ': Searched someones name'
+    files = ["File1.txt", "File2.txt"]
+    for i in files:
+        mc = open(i).readlines()
+    match = next((line for line in mc if name.lower() in line.lower()), None) # this returns the first line that has the provided name inside i
+    embed = discord.Embed(title=f'''{ctx.author} Searched Name''',  description=f'''{match}''', color=discord.Colour.blue())
+    await ctx.send(embed=embed)
 
 bot.run('NTU3ODM5ODcxNzQyNTc0NjE0.D3ONFQ.bw60zevXJO-k28s08Te_aeWh_qo')
