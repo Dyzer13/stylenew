cc716cf7b9b9658287fb9e9a78b67b2fa5edb0aa
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2019-03-20 03:51:20 -0700

    Update music.py

--- music.py
+++ music.py
@@ -1,13 +1,21 @@
 import discord
+from discord.ext.commands import Bot
 from discord.ext import commands
+import asyncio
+import time
 
-bot = commands.Bot(command_prefix='>')
+Client = discord.Client() 
+client = commands.Bot(command_prefix = ">")
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@client.event 
+async def on_ready():
+    print("Bot is online and connected to Discord")
 
-   
+@client.event
+async def on_message(message):
+    if message.content == "Money":
+        await client.send_message(message.channel, ":money_with_wings") 
+
 
 
 bot.run('NTU3ODM5ODcxNzQyNTc0NjE0.D3ONFQ.bw60zevXJO-k28s08Te_aeWh_qo')
